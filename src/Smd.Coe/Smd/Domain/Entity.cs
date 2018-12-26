31e32180f822f4c0e33884ec7c974fbf89b94925
guocheng <goodcode@yeah.net>
2018-12-26 18:28:06 +0800

    dev

--- src/Smd.Coe/Smd/Domain/Entity.cs
+++ src/Smd.Coe/Smd/Domain/Entity.cs
@@ -1,10 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
-namespace Smd.Smd.Domain
+namespace Smd.Domain
 {
-    class Entity
+    public abstract class Entity<TPrimaryKey> : IEntity<TPrimaryKey>
     {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public virtual TPrimaryKey Id { get; set; }
+
+        /// <summary>
+        /// 当前实体是否是临时的(没有持久化到数据库，没有Id值)
+        /// </summary>
+        /// <returns></returns>
+        public virtual bool IsTransient()
+        {
+            if (EqualityComparer<TPrimaryKey>.Default.Equals(Id, default(TPrimaryKey)))
+                return true;
+
+            //Workaround for EF Core since it sets int/long to min value when attaching to dbcontext
+            if (typeof(TPrimaryKey) == typeof(int))
+            {
+                return Convert.ToInt32(Id) <= 0;
+            }
+
+            if (typeof(TPrimaryKey) == typeof(long))
+            {
+                return Convert.ToInt64(Id) <= 0;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 重写是否相等
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity<TPrimaryKey>))
+            {
+                return false;
+            }
+
+            //相同引用肯定是相等的
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            //同时都为临时对象，被认为不相等
+            var other = (Entity<TPrimaryKey>)obj;
+            if (IsTransient() && other.IsTransient())
+            {
+                return false;
+            }
+
+            //Must have a IS-A relation of types or must be same type
+            var typeOfThis = GetType();
+            var typeOfOther = other.GetType();
+            if (!typeOfThis.GetTypeInfo().IsAssignableFrom(typeOfOther) && !typeOfOther.GetTypeInfo().IsAssignableFrom(typeOfThis))
+            {
+                return false;
+            }
+
+
+            return Id.Equals(other.Id);
+
+        }
+
+        public override int GetHashCode()
+        {
+            if (Id == null)
+            {
+                return 0;
+            }
+
+            return Id.GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public static bool operator ==(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+        {
+            if (Equals(left, null))
+            {
+                return Equals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        /// <inheritdoc/>
+        public static bool operator !=(Entity<TPrimaryKey> left, Entity<TPrimaryKey> right)
+        {
+            return !(left == right);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return $"[{GetType().Name} {Id}]";
+        }
     }
 }
